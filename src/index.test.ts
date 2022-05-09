03b0f8f3980fa3e1be8c7342b4a0779c5c504b01
Tom Yeoman <tom.yeoman@live.co.uk>
2022-05-09 18:31:55 +0100

    Format test file

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,9 +1,7 @@
-/* eslint @typescript-eslint/explicit-function-return-type: 0 */
+import { add } from ".";
 
-import { add } from './index';
-
-describe('add', () => {
-  test('should add 2 numbers', () => {
+describe("add", () => {
+  test("should add 2 numbers", () => {
     expect(add(1, 2)).toEqual(3);
   });
 });
